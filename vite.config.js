0321acbf567ab9b19c6db16bf706c9e1ac2b68fa
Porovozik91 <adam01@live.no>
2025-06-19 17:21:30 +0200

    Improved responsive design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['ce4e-84-214-60-59.ngrok-free.app'], 
+  },
 })
